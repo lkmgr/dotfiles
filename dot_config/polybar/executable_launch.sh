4eb47c1f9014a73c04d5f141cfab8fd4c7eedcf6
xmd3 <7467162+xmd3@users.noreply.github.com>
2021-12-28 10:31:58 +0100

    Add dot_config/polybar/blocks/config.ini Add dot_config/polybar/blocks/modules.ini Add dot_config/polybar/config.ini Update dot_config/polybar/executable_launch.sh Add dot_config/polybar/modules.ini

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -2,9 +2,24 @@
 
 killall -q polybar
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar --reload main & disown
-done
+while pgrep -x polybar > /dev/null; do sleep 0.25; done
 
-# polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+echo "--- RESTART ---" | tee -a /tmp/polybar.log
+
+PB_CONFIG=$HOME/.config/polybar/config.ini
+
+MONITOR=HDMI-0 polybar --reload --config=$PB_CONFIG main 2>&1 | tee -a /tmp/polybar.log & disown
+MONITOR=DP-0 polybar --reload --config=$PB_CONFIG secondary 2>&1 | tee -a /tmp/polybar.log & disown
+
+# MONITOR=HDMI-0 polybar --reload --config=$PB_CONFIG main_left 2>&1 | tee -a /tmp/polybar.log & disown
+# MONITOR=HDMI-0 polybar --reload --config=$PB_CONFIG main_center 2>&1 | tee -a /tmp/polybar.log & disown
+# MONITOR=HDMI-0 polybar --reload --config=$PB_CONFIG main_right 2>&1 | tee -a /tmp/polybar.log & disown
+# MONITOR=DP-0 polybar --reload --config=$PB_CONFIG secondary_left 2>&1 | tee -a /tmp/polybar.log & disown
+# MONITOR=DP-0 polybar --reload --config=$PB_CONFIG secondary_center 2>&1 | tee -a /tmp/polybar.log & disown
+# MONITOR=DP-0 polybar --reload --config=$PB_CONFIG secondary_right 2>&1 | tee -a /tmp/polybar.log & disown
+
+# Same bar on all monitors
+# for m in $(polybar --list-monitors | cut -d":" -f1); do
+#   MONITOR=$m polybar --reload main & disown
+# done
 
