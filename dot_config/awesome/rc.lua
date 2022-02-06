fd4d83eedda2d1a7e6cb2793d23d90670599952e
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-02-06 13:16:44 +0100

    Add dot_config/awesome/rc.lua

---
+++ dot_config/awesome/rc.lua
@@ -0,0 +1,15 @@
+-- awesome_mode: api-level=4:screen=on
+pcall(require, "luarocks.loader")
+
+local awful = require("awful")
+local beautiful = require("beautiful")
+local gears = require("gears")
+
+beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/custom/theme.lua")
+
+require("user.bindings")
+require("user.rules")
+require("user.signals")
+
+awful.spawn.with_shell("/home/lukas/.config/awesome/autostart.sh")
+
