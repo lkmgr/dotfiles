017cf9a39dcc3ed90f600e4bb5895df224ac3e68
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-02-09 21:23:25 +0100

    lint: fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,5 @@
 # if status is-interactive
-    # Commands to run in interactive sessions can go here
+# Commands to run in interactive sessions can go here
 # end
 
 # for file in $__fish_config_fir/custom.d/*.fish
@@ -7,18 +7,18 @@
 # end
 
 function l --wraps exa --description 'ls with exa'
-  if type -q exa
-    exa --long --color-scale --icons --all --all --group --header --group-directories-first $argv
-  else
-    ls -alh $argv
-  end
+    if type -q exa
+        exa --long --color-scale --icons --all --all --group --header --group-directories-first $argv
+    else
+        ls -alh $argv
+    end
 end
 
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 set -gx GPG_TTY (tty)
 
-abbr g 'git'
+abbr g git
 abbr ga 'git add'
 abbr gs 'git status'
 abbr gc 'git commit'
@@ -38,12 +38,11 @@ abbr bli 'brew list'
 
 abbr npr 'npm run'
 
-abbr v 'nvim'
+abbr v nvim
 
-abbr k 'kubectl'
-abbr kie 'kubie'
+abbr k kubectl
+abbr kie kubie
 
 if type -q starship
-  starship init fish | source
+    starship init fish | source
 end
-
