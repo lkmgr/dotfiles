27c54a2c0e216d9de0a0a85f02101fd723d043ed
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-02-09 09:32:11 +0100

    Update fish and starship

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,3 +6,23 @@ if type -q starship
   starship init fish | source
 end
 
+abbr g 'git'
+abbr ga 'git add'
+abbr gs 'git status'
+abbr gc 'git commit'
+abbr gco 'git checkout'
+abbr gp 'git push'
+abbr gpl 'git pull'
+abbr gcl 'git clone'
+abbr gf 'git fetch'
+abbr gdiff 'git diff'
+abbr glog 'git log'
+
+abbr bi 'brew install'
+abbr bse 'brew search'
+abbr bud 'brew update'
+abbr bug 'brew upgrade'
+abbr bli 'brew list'
+
+abbr npr 'npm run'
+
