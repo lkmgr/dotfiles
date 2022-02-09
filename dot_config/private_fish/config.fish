d805e8976f11fcb9f90b4a1da31153518c7babee
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-02-09 21:02:35 +0100

    Update fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,10 +1,22 @@
-if status is-interactive
+# if status is-interactive
     # Commands to run in interactive sessions can go here
+# end
+
+# for file in $__fish_config_fir/custom.d/*.fish
+#   source $file
+# end
+
+function l --wraps exa --description 'ls with exa'
+  if type -q exa
+    exa --long --color-scale --icons --all --all --group --header --group-directories-first $argv
+  else
+    ls -alh $argv
+  end
 end
 
-if type -q starship
-  starship init fish | source
-end
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx GPG_TTY (tty)
 
 abbr g 'git'
 abbr ga 'git add'
@@ -25,4 +37,13 @@ abbr bug 'brew upgrade'
 abbr bli 'brew list'
 
 abbr npr 'npm run'
+
+abbr v 'nvim'
+
+abbr k 'kubectl'
+abbr kie 'kubie'
+
+if type -q starship
+  starship init fish | source
+end
 
