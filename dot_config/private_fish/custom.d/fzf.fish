b87a258657764a1d2cf89c5e5552db6c17cc554d
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-03-03 08:52:13 +0100

    fix: fd ignore file

--- dot_config/private_fish/custom.d/fzf.fish
+++ dot_config/private_fish/custom.d/fzf.fish
@@ -2,6 +2,7 @@ if type -q exa
     set fzf_preview_dir_cmd exa --all --group-directories-first --icons --color=always
 end
 
-set fzf_fd_opts --hidden --exclude=.git --exclude=node_modules --exclude=.cache
+# excludes in ~/.config/fd/ignore
+set fzf_fd_opts --hidden
 
 fzf_configure_bindings --directory=\ef --git_log=\e\cl --git_status=\es --history=\er --variables=\ev --processes=\e\cp
