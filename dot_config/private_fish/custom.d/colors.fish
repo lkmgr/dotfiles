dd678690585d4ae7e416bab19a2b49f1c85caaba
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-03-21 23:27:51 +0100

    feat: tokyonight colorscheme

--- dot_config/private_fish/custom.d/colors.fish
+++ dot_config/private_fish/custom.d/colors.fish
@@ -1,46 +1,34 @@
-# Catppuccin color palette
+# TokyoNight Color Palette
+set -l foreground c0caf5
+set -l selection 364A82
+set -l comment 565f89
+set -l red f7768e
+set -l orange ff9e64
+set -l yellow e0af68
+set -l green 9ece6a
+set -l purple 9d7cd8
+set -l cyan 7dcfff
+set -l pink bb9af7
 
-# --> special
-set -l foreground dadae8
-set -l selection 3e4058
-
-# --> palette
-set -l teal bee4ed
-set -l flamingo f2cecf
-set -l magenta c6aae8
-set -l pink e5b4e2
-set -l red e38c8f
-set -l peach f9c096
-set -l green b1e3ad
-set -l yellow ebddaa
-set -l blue a4b9ef
-set -l gray 6e6c7e
-
-# Syntax Highlighting
+# Syntax Highlighting Colors
 set -g fish_color_normal $foreground
-set -g fish_color_command $green
-set -g fish_color_param $flamingo
-set -g fish_color_keyword $red
-set -g fish_color_quote $green
-set -g fish_color_redirection $pink
-set -g fish_color_end $peach
+set -g fish_color_command $cyan
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
 set -g fish_color_error $red
-set -g fish_color_gray $gray
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
 set -g fish_color_selection --background=$selection
 set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $pink
-set -g fish_color_escape $flamingo
-set -g fish_color_autosuggestion $gray
-set -g fish_color_cancel $red
+set -g fish_color_operator $green
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
 
-# Prompt
-set -g fish_color_cwd $yellow
-set -g fish_color_user $teal
-set -g fish_color_host $blue
-
-# Completion Pager
-set -g fish_pager_color_progress $gray
-set -g fish_pager_color_prefix $pink
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
 set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $gray
+set -g fish_pager_color_description $comment
 
