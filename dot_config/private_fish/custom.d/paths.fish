fe7bd077f4ba4cb92dde759fd9d1aebe6f14a966
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-03-22 09:37:36 +0100

    add ruby gem path and lunarvim config

--- dot_config/private_fish/custom.d/paths.fish
+++ dot_config/private_fish/custom.d/paths.fish
@@ -2,3 +2,8 @@ fish_add_path ~/.local/bin
 fish_add_path ~/.npm-global/bin
 fish_add_path ~/go/bin
 fish_add_path /usr/local/sbin
+
+if type -q gem; and test -d (gem environment gemdir)
+  fish_add_path (gem environment gemdir)/bin
+end
+
