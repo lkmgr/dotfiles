8f29586e446aae1d3956bd0fe56c4ab4141258b7
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-04-16 13:12:58 +0200

    feat: fish and aqua setup

--- dot_config/private_fish/custom.d/paths.fish
+++ dot_config/private_fish/custom.d/paths.fish
@@ -3,11 +3,15 @@ fish_add_path ~/.npm-global/bin
 fish_add_path ~/go/bin
 fish_add_path /usr/local/sbin
 
-if type -q gem; and test -d (gem environment gemdir)
-  fish_add_path (gem environment gemdir)/bin
+if type -q aqua
+  fish_add_path ~/.local/share/aqua/bin
 end
 
-if type -q ruby; and type -q gem
-  fish_add_path (ruby -e 'puts Gem.user_dir')/bin
-end
+# if type -q gem; and test -d (gem environment gemdir)
+#   fish_add_path (gem environment gemdir)/bin
+# end
+
+# if type -q ruby; and type -q gem
+#   fish_add_path (ruby -e 'puts Gem.user_dir')/bin
+# end
 
