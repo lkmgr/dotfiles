e32e90393b7c188f15329a7d4171fd589c608230
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-04-20 17:49:53 +0200

    update cd function

--- dot_config/private_fish/custom_functions.d/cd.fish
+++ dot_config/private_fish/custom_functions.d/cd.fish
@@ -3,7 +3,7 @@ function cd
     # if type -q exa
     #     exa --all --group-directories-first --icons
     if type -q lsd
-        lsd --all --group-dirs first --color always --icon always
+        lsd --almost-all --group-dirs first --color always --icon always
     else
         ls -ah
     end
