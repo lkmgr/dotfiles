093eb6a7b52e84bc2f7495753f7be1aa86c09420
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-04-25 09:30:42 +0200

    fish: add ruby PATH

--- dot_config/private_fish/custom.d/paths.fish
+++ dot_config/private_fish/custom.d/paths.fish
@@ -3,6 +3,11 @@ fish_add_path ~/.npm-global/bin
 fish_add_path ~/go/bin
 fish_add_path /usr/local/sbin
 
+if type -q ruby
+  fish_add_path /usr/local/lib/ruby/gems/3.1.0/bin
+  fish_add_path ~/.gem/ruby/3.1.0/bin
+end
+
 if type -q aqua
   fish_add_path ~/.local/share/aqua/bin
 end
@@ -14,4 +19,3 @@ end
 # if type -q ruby; and type -q gem
 #   fish_add_path (ruby -e 'puts Gem.user_dir')/bin
 # end
-
