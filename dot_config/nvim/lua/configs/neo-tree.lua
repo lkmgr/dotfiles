36b4dfb174e06095fe000b9e1736f558d1b7a9ee
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-04-26 10:30:46 +0200

    nvim: show dotfiles in neotree

--- dot_config/nvim/lua/configs/neo-tree.lua
+++ dot_config/nvim/lua/configs/neo-tree.lua
@@ -102,7 +102,7 @@ function M.config()
     filesystem = {
       filtered_items = {
         visible = true, -- when true, they will just be displayed differently than normal items
-        hide_dotfiles = true,
+        hide_dotfiles = false,
         hide_gitignored = true,
         hide_hidden = true, -- only works on Windows for hidden files/directories
         hide_by_name = {
