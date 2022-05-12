8a44255843f253f78afb361aa331fc071374d3f2
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-05-12 08:26:24 +0200

    nvim: lualine updates

--- dot_config/nvim/lua/configs/lualine.lua
+++ dot_config/nvim/lua/configs/lualine.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.config()
-  local status_ok, lualine = pcall(require, 'lualine')
+  local status_ok, lualine = pcall(require, "lualine")
   if not status_ok then
     return
   end
@@ -9,33 +9,32 @@ function M.config()
   lualine.setup {
     options = {
       icons_enabled = true,
-      theme = 'auto',
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
+      theme = "auto",
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
       disabled_filetypes = { "NvimTree", "neo-tree", "dashboard", "Outline", "alpha" },
       always_divide_middle = true,
       globalstatus = false,
     },
     sections = {
-      lualine_a = {'mode'},
-      lualine_b = {'branch', 'diff', 'diagnostics'},
-      lualine_c = {'filename'},
-      lualine_x = {'encoding', 'fileformat', 'filetype'},
-      lualine_y = {'progress'},
-      lualine_z = {'location'}
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { { "filename", path = 1, shorting_target = 50 } },
+      lualine_x = { "encoding", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
     },
     inactive_sections = {
       lualine_a = {},
       lualine_b = {},
-      lualine_c = {'filename'},
-      lualine_x = {'location'},
+      lualine_c = { { "filename", path = 1, shorting_target = 60 } },
+      lualine_x = { "location" },
       lualine_y = {},
-      lualine_z = {}
+      lualine_z = {},
     },
     tabline = {},
-    extensions = {}
+    extensions = { "neo-tree", "quickfix", "toggleterm" },
   }
 end
 
 return M
-
