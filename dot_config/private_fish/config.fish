888d749a1c07a0bf681e9f5240023ad99453f1d4
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-05-14 14:49:37 +0200

    fish: remove fzf colors

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,11 +1,11 @@
 # custom configs
 for file in $__fish_config_dir/custom.d/*.fish
-    source $file
+  source $file
 end
 
 # prepend custom function path to fish_function_path
 if test -d $__fish_config_dir/custom_functions.d
-    set -gp fish_function_path $__fish_config_dir/custom_functions.d
+  set -gp fish_function_path $__fish_config_dir/custom_functions.d
 end
 
 # if type -q asdf; and test -f /usr/local/opt/asdf/libexec/asdf.fish
@@ -13,7 +13,7 @@ end
 # end
 
 if type -q starship
-    starship init fish | source
+  starship init fish | source
 end
 
 # if type -q keychain
@@ -21,5 +21,5 @@ end
 # end
 
 if type -q direnv
-    direnv hook fish | source
+  direnv hook fish | source
 end
--- dot_config/private_fish/custom.d/fzf.fish
+++ dot_config/private_fish/custom.d/fzf.fish
@@ -10,5 +10,6 @@ set fzf_fd_opts --hidden
 
 fzf_configure_bindings --directory=\ef --git_log=\e\cl --git_status=\es --history=\er --variables=\ev --processes=\e\cp
 
-set -Ux FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*" --color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD'
+# set -Ux FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*" --color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD'
+set -Ux FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
 
