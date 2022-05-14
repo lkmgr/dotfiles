ac899375084bbbeec41c8e77a49beb6d867c9772
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-05-14 17:01:49 +0200

    fish: fzf base16 colors

--- dot_config/private_fish/custom.d/fzf.fish
+++ dot_config/private_fish/custom.d/fzf.fish
@@ -11,4 +11,7 @@ set fzf_fd_opts --hidden
 fzf_configure_bindings --directory=\ef --git_log=\e\cl --git_status=\es --history=\er --variables=\ev --processes=\e\cp
 
 set -gx FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
+set -a FZF_DEFAULT_OPTS "--color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D"
+set -a FZF_DEFAULT_OPTS "--color=fg:$color06,header:$color0D,info:$color0A,pointer:$color0C"
+set -a FZF_DEFAULT_OPTS "--color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
 
