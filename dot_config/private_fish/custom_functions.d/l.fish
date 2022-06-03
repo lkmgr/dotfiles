6a7519f5c235f0982089c400f1998fdc57c7b331
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-06-03 23:47:14 +0200

    lsd: add config and theme

--- dot_config/private_fish/custom_functions.d/l.fish
+++ dot_config/private_fish/custom_functions.d/l.fish
@@ -1,8 +1,6 @@
 function l --wraps lsd --description 'ls with lsd'
-    # if type -q exa
-        # exa --long --color-scale --icons --all --all --group --header --group-directories-first $argv
     if type -q lsd
-        lsd --all --long --group-dirs first --color always --icon always $argv
+        lsd --all --long $argv
     else
         ls -alh $argv
     end
--- dot_config/private_fish/custom_functions.d/ll.fish
+++ dot_config/private_fish/custom_functions.d/ll.fish
@@ -1,8 +1,6 @@
 function ll --wraps lsd
-    # if type -q exa
-    #     exa --all --group-directories-first --icons $argv
     if type -q lsd
-        lsd --almost-all --group-dirs first --color always --icon always $argv
+        lsd --almost-all $argv
     else
         ls -ah $argv
     end
