51a80497c1121077f997e900bd7ef50bc98bbe25
xmd3 <7467162+xmd3@users.noreply.github.com>
2022-06-04 23:07:37 +0200

    qtile: add rules

--- dot_config/qtile/modules/groups.py
+++ dot_config/qtile/modules/groups.py
@@ -1,11 +1,26 @@
-from libqtile.config import Group, Key, ScratchPad, DropDown
+from libqtile.config import Group, Key, ScratchPad, DropDown, Match, Rule
 from libqtile.lazy import lazy
 from .keys import keys, mod
 
 usable_group_keys = [i for i in "123456789"]
 # groups = [Group(i) for i in "123456789"]
-groups = [Group(i, layout="columns") for i in "12345"]
-groups.extend([Group(i, layout="column") for i in "6789"])
+# groups = [Group(i, layout="columns") for i in "12345"]
+# groups.extend([Group(i, layout="column") for i in "6789"])
+
+# def class_matches(classes):
+#     return [Match(wm_class=[x]) for x in classes]
+
+groups = [
+    Group("1", layout="columns", matches=[Match(wm_class=["firefox"])]),
+    Group("2", layout="columns"),
+    Group("3", layout="columns", matches=[Match(wm_class=["Steam"])]),
+    Group("4", layout="columns", matches=[Match(wm_class=["Lens"])]),
+    Group("5", layout="columns", matches=[Match(wm_class=["discord"])]),
+    Group("6", layout="column", matches=[Match(wm_class=["chromium"])]),
+    Group("7", layout="column", matches=[Match(wm_class=["Thunderbird"])]),
+    Group("8", layout="column"),
+    Group("9", layout="column"),
+]
 
 for i, group in enumerate(groups):
     keys.append(
--- dot_config/qtile/modules/layouts.py
+++ dot_config/qtile/modules/layouts.py
@@ -1,4 +1,5 @@
 from libqtile import layout
+from libqtile.config import Match
 from .vars import gap, theme
 
 border_width = 6
@@ -72,6 +73,10 @@ floating_layout = layout.Floating(
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
+        Match(wm_class="Lutris"),
+        Match(wm_class="Steam"),
+        Match(wm_class="Bitwarden"),
+        Match(wm_class="battle.net.exe"),
         # Match(wm_class="confirmreset"),  # gitk
         # Match(wm_class="makebranch"),  # gitk
         # Match(wm_class="maketag"),  # gitk
