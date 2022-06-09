38466b87c266cb64ff35567a9e4b29e61ba0e78e
dexvm <7467162+dexvm@users.noreply.github.com>
2022-06-09 23:15:31 +0200

    chore: delete unused files

--- dot_config/lvim/lua/user/keymaps.lua
+++
@@ -1,8 +0,0 @@
--- keymappings [view all the defaults by pressing <leader>Lk]
-lvim.leader = "space"
--- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.insert_mode["jj"] = false
-lvim.keys.insert_mode["jk"] = false
-lvim.keys.insert_mode["kj"] = false
-
