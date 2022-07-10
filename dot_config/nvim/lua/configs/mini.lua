efa055704a23ca9b6fc13dd9a9b4ed1c71f57b1a
lkmgr <7467162+lkmgr@users.noreply.github.com>
2022-07-10 14:19:08 +0200

    nvim: remove mini sessions

--- dot_config/nvim/lua/configs/mini.lua
+++ dot_config/nvim/lua/configs/mini.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.config()
-  local configs = { "bufremove", "sessions", "starter", "surround", "trailspace" }
+  local configs = { "bufremove", "starter", "surround", "trailspace" }
 
   for _, config in ipairs(configs) do
     local ok, loaded = pcall(require, "configs.mini." .. config)
