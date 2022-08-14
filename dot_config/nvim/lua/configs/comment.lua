dd692664afd22c03369b73d0873d562f20b7185c
lkmgr <7467162+lkmgr@users.noreply.github.com>
2022-08-14 19:31:04 +0200

    nvim: make comment.nvim mappings in setup explicit

--- dot_config/nvim/lua/configs/comment.lua
+++ dot_config/nvim/lua/configs/comment.lua
@@ -7,18 +7,36 @@ function M.config()
   end
 
   comment.setup {
+    toggler = {
+      line = "gcc",
+      block = "gbc",
+    },
+    opleader = {
+      line = "gc",
+      block = "gb",
+    },
+    extra = {
+      above = "gcO",
+      below = "gco",
+      eol = "gcA",
+    },
+    mappings = {
+      basic = true,
+      extra = true,
+      extended = false,
+    },
     pre_hook = function(ctx)
-      local U = require 'Comment.utils'
+      local U = require "Comment.utils"
 
       local location = nil
       if ctx.ctype == U.ctype.block then
-        location = require('ts_context_commentstring.utils').get_cursor_location()
+        location = require("ts_context_commentstring.utils").get_cursor_location()
       elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = require('ts_context_commentstring.utils').get_visual_start_location()
+        location = require("ts_context_commentstring.utils").get_visual_start_location()
       end
 
-      return require('ts_context_commentstring.internal').calculate_commentstring {
-        key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
+      return require("ts_context_commentstring.internal").calculate_commentstring {
+        key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
         location = location,
       }
     end,
@@ -26,4 +44,3 @@ function M.config()
 end
 
 return M
-
