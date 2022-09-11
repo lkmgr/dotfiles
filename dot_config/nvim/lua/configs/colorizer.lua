35f0dc31143f0a1bf9b966b1b0af69f4f8509414
lkmgr <7467162+lkmgr@users.noreply.github.com>
2022-09-11 13:58:59 +0200

    nvim: update colorizer config

--- dot_config/nvim/lua/configs/colorizer.lua
+++ dot_config/nvim/lua/configs/colorizer.lua
@@ -11,7 +11,7 @@ function M.config()
     user_default_options = {
       RGB = true, -- #RGB hex codes
       RRGGBB = true, -- #RRGGBB hex codes
-      names = true, -- "Name" codes like Blue or blue
+      names = false, -- "Name" codes like Blue or blue
       RRGGBBAA = true, -- #RRGGBBAA hex codes
       AARRGGBB = false, -- 0xAARRGGBB hex codes
       rgb_fn = false, -- CSS rgb() and rgba() functions
