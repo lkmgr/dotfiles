eb23b1d4db91ab4db9f24c7aa221eafe069f7d92
lkmgr <7467162+lkmgr@users.noreply.github.com>
2023-01-23 01:15:03 +0100

    nvim: add gitsigns

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -153,4 +153,21 @@ return {
       require("mini.cursorword").setup()
     end,
   },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre",
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "契" },
+        topdelete = { text = "契" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      -- Set up keymappings
+      -- on_attach = function(buffer) end,
+    },
+  },
 }
