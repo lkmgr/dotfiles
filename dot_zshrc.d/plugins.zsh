6eb44eeab51a7f6e8be276a1fc9d262043c98c28
lkmgr <7467162+lkmgr@users.noreply.github.com>
2023-04-04 01:35:38 +0200

    zsh: create plugins.zsh

---
+++ dot_zshrc.d/plugins.zsh
@@ -0,0 +1,14 @@
+plug "zsh-users/zsh-autosuggestions"
+# plug "marlonrichert/zsh-autocomplete"
+plug "zsh-users/zsh-completions"
+
+plug "olets/zsh-abbr"
+
+plug "Aloxaf/fzf-tab"
+
+plug "romkatv/powerlevel10k"
+
+# last plugins to load
+plug "zdharma-continuum/fast-syntax-highlighting"
+plug "zsh-users/zsh-history-substring-search"
+
