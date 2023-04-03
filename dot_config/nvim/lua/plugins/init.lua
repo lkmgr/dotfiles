2195d0d72e24bb0263efd82720d6d86e43dd4715
lkmgr <7467162+lkmgr@users.noreply.github.com>
2023-04-04 01:34:40 +0200

    nvim: use catppuccin

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -27,13 +27,89 @@ return {
     opts = { default = true },
   },
 
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("tokyonight").setup()
+  --     vim.cmd [[colorscheme tokyonight]]
+  --   end,
+  -- },
+
   {
-    "folke/tokyonight.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
-    priority = 1000,
     config = function()
-      require("tokyonight").setup()
-      vim.cmd [[colorscheme tokyonight]]
+      require("catppuccin").setup {
+        flavour = "mocha",
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = false,
+        show_end_of_buffer = false,
+        term_colors = false,
+        dim_inactive = {
+          enabled = false,
+          shade = "dark",
+          percentage = 0.15,
+        },
+        no_italic = false,
+        no_bold = false,
+        styles = {
+          comments = { "italic" },
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        integrations = {
+          gitsigns = true,
+          leap = true,
+          markdown = true,
+          mason = true,
+          mini = true,
+          neotree = true,
+          cmp = true,
+          notify = true,
+          -- treesitter_context = true,
+          treesitter = true,
+          telescope = true,
+          which_key = true,
+          -- special
+          indent_blankline = {
+            enabled = true,
+            -- colored_indent_levels = true,
+          },
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+            },
+          },
+        },
+      }
+      vim.cmd.colorscheme "catppuccin"
     end,
   },
 
@@ -59,7 +135,11 @@ return {
 
   {
     "nvim-lualine/lualine.nvim",
-    config = true,
+    config = {
+      options = {
+        theme = "catppuccin",
+      },
+    },
   },
 
   {
