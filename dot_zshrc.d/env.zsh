a04ae6c14f881ad6d85541bdb0f57e75a6d4348f
lkmgr <7467162+lkmgr@users.noreply.github.com>
2023-05-12 09:41:48 +0200

    zsh: fzf colors

--- dot_zshrc.d/env.zsh
+++ dot_zshrc.d/env.zsh
@@ -22,3 +22,8 @@ if command_exists aqua; then
   export AQUA_GLOBAL_CONFIG="$HOME/.config/aqua/aqua.yaml"
   export AQUA_ROOT_DIR="$HOME/.local/share/aqua"
 fi
+
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
+--color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
