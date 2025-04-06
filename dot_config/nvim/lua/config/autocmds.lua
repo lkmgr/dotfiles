1a2d7878f133e78b30be3e9c4d307c7a1c398c42
lkmgr <7467162+lkmgr@users.noreply.github.com>
2025-04-06 12:44:16 +0200

    nvim: add fold options

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -15,3 +15,14 @@ vim.api.nvim_create_autocmd({ "BufWinEnter", "BufRead", "BufNewFile" }, {
   end,
   pattern = "*",
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client:supports_method("textDocument/foldingRange") then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+    end
+  end,
+})
+vim.api.nvim_create_autocmd("LspDetach", { command = "setl foldexpr<" })
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -8,3 +8,11 @@ vim.g.lazyvim_picker = "fzf"
 
 vim.opt.scrolloff = 8
 vim.opt.conceallevel = 0
+
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldtext = ""
+vim.opt.foldcolumn = "0"
+vim.opt.fillchars:append({ fold = " " })
